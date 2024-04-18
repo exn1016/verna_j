a691ab6e3d36b29e5b273938001cb6c4bb518b74
nengxiong <nengxiong@tencent.com>
2024-04-18 14:35:49 +0800

    init

---
+++ qiao/src/main/java/org/kakuma/qiao/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.kakuma.qiao;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(QiaoApplication.class);
+    }
+
+}
---
+++ qiao/src/test/java/org/kakuma/qiao/QiaoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kakuma.qiao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QiaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
